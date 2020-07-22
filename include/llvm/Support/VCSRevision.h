2b58a276d8831e493f70656b234e7067b9868670
ZyCromerZ <neetroid97@gmail.com>
2020-07-23 04:23:40 +0700

    9.0.4-android-10.0.0_r40-clang-r353983d
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

--- include/llvm/Support/VCSRevision.h
+++ include/llvm/Support/VCSRevision.h
@@ -1,2 +1,2 @@
-#define LLVM_REVISION "2a90cc9d899e6fe6dcf0da193244b25f75735c7b"
-#define LLVM_REPOSITORY "/usr/local/google/buildbot/src/android/llvm-toolchain/toolchain/llvm-project/llvm"
+#define LLVM_REVISION "31c3f8c4ae6cc980405a3b90e7e88db00249eba5"
+#define LLVM_REPOSITORY "/buildbot/src/android/llvm-r353983/toolchain/llvm"
